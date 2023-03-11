98cdab5eb3b9b34aa9c497b0efcad89a0692275e
Richard Abhulimhen <richardabhulimhen@gmail.com>
2023-03-11 16:38:20 -0500

    Start new note view

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -3,7 +3,7 @@ import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:mynotes/services/auth/auth_service.dart';
 import 'package:mynotes/views/login_view.dart';
-import 'package:mynotes/views/notes_view.dart';
+import 'package:mynotes/views/notes/notes_view.dart';
 import 'package:mynotes/views/verify_email_view.dart';
 
 const tag = "HomePage";
---
+++ lib/views/notes/new_note_view.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class NewNoteView extends StatefulWidget {
+  const NewNoteView({super.key});
+
+  @override
+  State<NewNoteView> createState() => _NewNoteViewState();
+}
+
+class _NewNoteViewState extends State<NewNoteView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("New Note"),
+      ),
+      body: const Text("Write your new note here.."),
+    );
+  }
+}
